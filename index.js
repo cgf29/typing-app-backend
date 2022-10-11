3b405727b74b8a2c88cd179deba4c1d1fe2185ff
Vanya Svyatun <vanyasv06@gmail.com>
2022-10-11 21:56:43 +0300

    3

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app = express()
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://jazzy-treacle-7c635b.netlify.app/");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
