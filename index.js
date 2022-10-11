dcfc41b8f6d81e684ed6c6a763a89bf1c90fb694
Vanya Svyatun <vanyasv06@gmail.com>
2022-10-11 22:11:29 +0300

    3

--- index.js
+++ index.js
@@ -27,32 +27,36 @@ setInterval(() => {
 
 
 app.get('/', async (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    const words = await axios.get('https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt')
-        .then(res => res.data.toString().replaceAll('\n', ' ').split(' '))
-    // const schema = await TextSchema.create({ text: ['t', 'b'] })
-    const words1 = []
-    const words2 = []
-    const words3 = []
-    const words4 = []
-    const words5 = []
-    for (let i = 0; i < 100; i++) {
-        if (i < 20 && i >= 0) {
-            words1.push(words[Math.floor(Math.random() * 1000)])
-        } else if (i < 40 && i >= 20) {
-            words2.push(words[Math.floor(Math.random() * 1000)])
-        } else if (i < 60 && i >= 40) {
-            words3.push(words[Math.floor(Math.random() * 1000)])
-        } else if (i < 80 && i >= 60) {
-            words4.push(words[Math.floor(Math.random() * 1000)])
-        } else if (i < 100 && i >= 80) {
-            words5.push(words[Math.floor(Math.random() * 1000)])
+    try {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        const words = await axios.get('https://gist.githubusercontent.com/deekayen/4148741/raw/98d35708fa344717d8eee15d11987de6c8e26d7d/1-1000.txt')
+            .then(res => res.data.toString().replaceAll('\n', ' ').split(' '))
+        // const schema = await TextSchema.create({ text: ['t', 'b'] })
+        const words1 = []
+        const words2 = []
+        const words3 = []
+        const words4 = []
+        const words5 = []
+        for (let i = 0; i < 100; i++) {
+            if (i < 20 && i >= 0) {
+                words1.push(words[Math.floor(Math.random() * 1000)])
+            } else if (i < 40 && i >= 20) {
+                words2.push(words[Math.floor(Math.random() * 1000)])
+            } else if (i < 60 && i >= 40) {
+                words3.push(words[Math.floor(Math.random() * 1000)])
+            } else if (i < 80 && i >= 60) {
+                words4.push(words[Math.floor(Math.random() * 1000)])
+            } else if (i < 100 && i >= 80) {
+                words5.push(words[Math.floor(Math.random() * 1000)])
+            }
         }
+        res.json({
+            status: 'ok', data: [words1, words2, words3, words4, words5]
+        })
+    } catch (err) {
+        res.send(err)
     }
-    res.json({
-        status: 'ok', data: [words1, words2, words3, words4, words5]
-    })
 })
 app.listen(8000, () => {
     console.log('ok')
