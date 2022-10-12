5ebc6e93123b0f96605143cf7485d0aa09c771f2
Vanya Svyatun <vanyasv06@gmail.com>
2022-10-12 12:34:18 +0300

    4

--- index.js
+++ index.js
@@ -5,21 +5,19 @@ import axios from 'axios';
 import TextSchema from './schemas/Text.js'
 
 const app = express()
-app.use(cors({
-    origin: 'http://localhost:3000/',
-    origin: 'https://jazzy-treacle-7c635b.netlify.app/'
-}))
 // app.use(cors({
 //     origin: 'https://jazzy-treacle-7c635b.netlify.app/',
 //     origin: 'http://localhost:3000'
 // }))
 
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Origin", "https://jazzy-treacle-7c635b.netlify.app/");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://jazzy-treacle-7c635b.netlify.app/");
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const url = 'mongodb+srv://admin:xEcZ4F2YPxwhAHQ0@cluster0.ppq7mkc.mongodb.net/?retryWrites=true&w=majority'
 
